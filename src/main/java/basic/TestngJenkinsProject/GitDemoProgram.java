069f94f5bbf7480345041ec3166a1375c4574abc
tejas.metre <tejas.metre@skillnetinc.com>
2024-12-16 20:40:03 +0530

    second commit

---
+++ src/main/java/basic/TestngJenkinsProject/GitDemoProgram.java
@@ -0,0 +1,10 @@
+package basic.TestngJenkinsProject;
+
+public class GitDemoProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello This is the first program");
+	}
+
+}
