5aeac65288e6a888cda8ba35fde7d65a8b212601
Tejas-Metre <tejasametre1@gmail.com>
2024-12-25 21:25:07 +0530

    third commit

--- src/test/java/demo/FirstTestngClass.java
+++ src/test/java/demo/FirstTestngClass.java
@@ -7,27 +7,21 @@ public class FirstTestngClass {
 	void method1()
 	{
 		System.out.println("it is in method1");
-				System.out.println("first method");
-
 	}
 	//@Test
 	void method2()
 	{
 		System.out.println("it is in method2");
-		System.out.println("second method");
 	}
 	@Test
 	void show()
 	{
 		System.out.println("It is in show");
-		System.out.println("This is first Testng Program");
 	}
 	@Test
 	void display()
 	{
 		System.out.println("It is in display");
-		System.out.println("This is first Testng Program");
-
 	}
 
 }
